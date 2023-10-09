7e2753476c07b72d5d10b7e2ae6988d667491ee5
oggnimodd <arnolddominggo19@gmail.com>
2023-10-09 10:46:39 +0700

    ui : remove all the components

--- packages/ui/components/Loading/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as Loading } from "./Loading";
-export { default as LoadingWithMessage } from "./LoadingWithMessage";
-
-export * from "./Loading";
-export * from "./LoadingWithMessage";
--- packages/ui/components/PageMessage/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as PageMessage } from "./PageMessage";
-
-export * from "./PageMessage";
